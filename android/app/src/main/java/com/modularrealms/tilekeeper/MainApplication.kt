24a7ab657988f7efb07b8703b477a532a6ade416
Rich (via Labby) <kalsk@outlook.com>
2026-06-08 18:47:49 +0100

    fix(MainApplication): add reactNativeHost implementation for RN 0.81 compat

--- android/app/src/main/java/com/modularrealms/tilekeeper/MainApplication.kt
+++ android/app/src/main/java/com/modularrealms/tilekeeper/MainApplication.kt
@@ -8,8 +8,10 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.ReactPackage
 import com.facebook.react.ReactHost
+import com.facebook.react.ReactNativeHost
 import com.facebook.react.common.ReleaseLevel
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint
+import com.facebook.react.defaults.DefaultReactNativeHost
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ExpoReactHostFactory
@@ -24,6 +26,14 @@ class MainApplication : Application(), ReactApplication {
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // add(MyReactNativePackage())
         }
+    )
+  }
+
+  @Suppress("DEPRECATION")
+  override val reactNativeHost: ReactNativeHost by lazy {
+    DefaultReactNativeHost.getDefaultReactNativeHost(
+      applicationContext,
+      PackageList(this).packages
     )
   }
 
